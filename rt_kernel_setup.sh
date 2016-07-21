727933f630e4ae125c063be5173e581e646fd2b6
Sabar Dasgupta <s@bardasgupta.com>
2016-07-21 15:47:34 -0700

    fix bash setup file
    Former-commit-id: f9cb5be1b3936a75270b17640edd2e903aba94b5

---
+++ rt_kernel_setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+sudo apt-get update && 
+sudo apt-get upgrade &&
+sudo apt-get install ncurses-dev &&
+sudo apt-get install build-essential &&
+wget ftp://ftp.kernel.org/pub/linux/kernel/v4.x/linux-4.4.12.tar.gz &&
+wget http://www.kernel.org/pub/linux/kernel/projects/rt/4.4/patch-4.4.12-rt19.patch.gz &&
+tar -zxvf linux-4.4.12.tar.gz &&
+cd linux-4.4.12 &&
+zcat ../patch-4.4.12-rt19.patch.gz | patch -p1 &&
+sudo apt-get install kernel-package libssl-dev &&
+make-kpkg clean &&
+fakeroot make-kpkg –-initrd –-append-to-version=-custom kernel_image kernel_headers &&
+sudo dpkg -i linux-image-4.4.12-custom-rt19_4.4.12-custom-rt19-10.00.Custom_amd64.deb &&
+sudo dpkg -i linux-headers-4.4.12-custom-rt19_4.4.12-custom-rt19-10.00.Custom_amd64.deb &&
+sudo reboot
