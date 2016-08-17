8ec0dea47844c8733ebff88513051a3458d637ec
Sabar Dasgupta <s@bardasgupta.com>
2016-08-17 13:09:13 -0700

    more work on logger. trying to get this to compile
    Former-commit-id: e66f135a6f42e3c65c35bd99c4194841bcb31990 [formerly 20f6e0aba4b0282d40bcc4a5d5d292dbfb60d3e8] [formerly a8fb0678304fc3becf0286077d03313c91b5869f [formerly b4a19fb007b8b81cc98b1d8d2019406aa9bae7fa]] [formerly 198691cab4490dfdf19c2c8550b7d72905a96e5c [formerly f519ced2f0f046a5788324f3a1002737df2707a5]]
    Former-commit-id: d1e8bf003c051bbb77ab9dd4a3b782262ac78a86 [formerly 198691cab4490dfdf19c2c8550b7d72905a96e5c]
    Former-commit-id: 30d4498f4fe3fbdc6db9eaac890d76a23cc8501b [formerly e7f1405cfea2ffa3126bffd3ba6f19527ac2498d]
    Former-commit-id: af38da0e63ca38ab5f25edc4a74463f73da28820

--- utilityFunctions.h
+++ utilityFunctions.h
@@ -3,9 +3,11 @@
 #ifndef _UTILITY_FUNCTIONS_
 #define _UTILITY_FUNCTIONS_ 
 
-void die(char *errorStr, void (*pHandleExit)());
+void init_utils(void (*pHandleExit)(int exitStatus));
 
-void open_shared_mem(int *pfd, uint8_t **ppmem, const char *pName, int numPages, int shm_flags, void (*pHandleExit)());
+void die(char *errorStr);
+
+void open_shared_mem(uint8_t **ppmem, const char *pName, int numPages, int shm_flags);
 
 void set_sighandler(int signum, void *psh);
 
