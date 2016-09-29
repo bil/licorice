8dec7b57e1f7033c1a84b52a3b0313c4e04e2be0
Sabar Dasgupta <s@bardasgupta.com>
2016-09-28 17:36:54 -0700

    add comments to utilityFunctions files
    Former-commit-id: de747a800b3629f0f8c49df335568ecb45b1db5a [formerly f6d158ad2fd83b7d41aff2086370c326f9b008ae] [formerly 0a0212801c9ac21400f5e3db532b744398d6cdcb [formerly 8aab4f790dafdf2d32e21172dcecb8604e671ec8]] [formerly f82dc3fbfc702649dfa209416c4f8615cef9e25d [formerly 702834010659b0631ad7806846968f490871faa0]]
    Former-commit-id: f82dc3fbfc702649dfa209416c4f8615cef9e25d
    Former-commit-id: d3c096bcbfb78377b402252e088c78001e785853 [formerly 2ce0bfbc31168769d89d27dba74fc61e2d26b184]
    Former-commit-id: 861bcf69edc67faee5f04898b4af5cfe8184d3d1

--- utilityFunctions.c
+++ utilityFunctions.c
@@ -11,16 +11,35 @@
 extern sigset_t exitMask;
 static void (*exit_handler)(int errorStr);
 
+
+/*
+* initialize utilities
+* set function to run on exit
+*/ 
 void init_utils(void (*pHandleExit)(int errorStr)) {
   exit_handler = pHandleExit;
 }
 
+/*
+* run the function specified by exit_handler and print the given error message
+*/
 void die(char *errorStr) {
+// void die(const char *format, ...) {  
   sigprocmask(SIG_BLOCK, &exitMask, NULL);
+/* can try to allow for printf-like argument as input
+  va_list argptr;
+  va_start(argptr, format);
+  vfprintf(stderr, format, argptr);
+  va_end(argptr);
+*/  
   perror(errorStr);
   exit_handler(1);
 }
 
+/* 
+* create a signal handler that handles signal signum and runs the function *psh
+* when signum is raised
+*/
 void set_sighandler(int signum, void *psh) {
   struct sigaction sa;
   memset(&sa, 0, sizeof(sa));
@@ -34,6 +53,16 @@ void set_sighandler(int signum, void *psh) {
   }
 }
 
+/*
+* open a shared memory block with:
+* name: pName
+* size: numPages * PAGESIZE
+* flags: shm_flags
+* *ppmem then points to the beginning of this block of memory once the function has run
+* 
+* If the file descriptor needs to be ftruncated (i.e., this is the first process opening)
+* this shared memory, then make sure the O_CREAT flag is set in shm_flags
+*/
 void open_shared_mem(uint8_t **ppmem, const char *pName, int numPages, int shm_flags) {
   int fd = shm_open(pName, shm_flags, 0600);
   if(fd == -1) {
