00bc3498589e4fb95ffac3a0365bb2117609585c
Sabar Dasgupta <s@bardasgupta.com>
2017-04-11 15:44:50 -0700

    simple one module templating working example
    Former-commit-id: 6456ebffc37a3fdd1929656117c0939413fdc3bd [formerly ccafaf25a17c4a1d082982e9fe5b5f81d03a4b2c] [formerly c4d122063a09cc83c61b2f0813f2fb2e33487695 [formerly b68b68d3f265ecc439f84c0cba4861ad1d8e2ca2]] [formerly fc4bc9ca01d9b27a4fd845a2d5e52a3ad20b9391 [formerly bfdf2c419a5511c09ba3b89ab39dc4498ecfc3d2]]
    Former-commit-id: fc4bc9ca01d9b27a4fd845a2d5e52a3ad20b9391
    Former-commit-id: 8f0745b847beaa8c976831e0dd1db736c0b89c95 [formerly 93e63a28b0bedb188f8d17c65b7199df472c12e1]
    Former-commit-id: 75fda52176c7982ff698ff0b9ca58b65ec63ced5

---
+++ templating/lico_parse.py
@@ -0,0 +1,30 @@
+import argparse
+import yaml
+from template_funcs import parse, generate
+
+# do some argument parsing
+arg_parser = argparse.ArgumentParser(description='LiCoRICE config parser.')
+arg_parser.add_argument('-c', '--config', type=file, help='config file to parse')
+arg_parser.add_argument('-g', '--generate', action='store_true', help='generate user code templates')
+args = arg_parser.parse_args()
+
+if args.config == None:
+  print "Must specify config file."
+  exit()
+
+config = yaml.safe_load(args.config)
+
+# this assumes that a top level object with three primary mappings is loaded
+# the only three mappings should be: types, signals, and modules
+# later versions should throw an error if this is not true
+# Relevant note: this entire parser is dangerous and does not have any safety checks
+#    it will break badly for malformed yaml data
+top_level = ['config', 'modules', 'signals', 'types']
+if (set(top_level) != set(config.keys())):
+  print("Invalid config file.")
+  exit()
+
+if args.generate == True:
+  generate(config)
+else: 
+  parse(config)
