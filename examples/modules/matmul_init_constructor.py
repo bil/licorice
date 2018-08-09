15bc3d69a556c685d5f6c4f7a21953fec4f34a91
Paul Nuyujukian <38359213+bil-paul@users.noreply.github.com>
2018-08-08 22:26:23 -0700

    add matrix multiply example - not fully functional

---
+++ examples/modules/matmul_init_constructor.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+init_done = 0
+m1_init = np.random.rand(4, 4)
+m2_init = np.random.rand(4, 4)
---
+++ examples/modules/matmul_print.py
@@ -0,0 +1,7 @@
+if counter < 2000:
+    counter += 1
+
+else:
+    counter = 0
+
+    print(matmul_out[:,:])
