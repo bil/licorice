7e1740abdc307e99d798f630211ba6b7609b3bca
Paul Nuyujukian <38359213+bil-paul@users.noreply.github.com>
2018-08-08 21:27:14 -0700

    update parallel_toggle model to work with updated codebase

--- examples/modules/parallel_toggler.py
+++ examples/modules/parallel_toggler.py
@@ -1,6 +1,13 @@
-if toggle_state == 0:
-    toggle_state = 1
-    out_sigs['parallel_sink_input'][0] = 0b10000000 # set pin 9 of parallel port to 1
+# Toggle pin 9 of the parallel port every two seconds
+
+if counter < 2000 :
+    counter += 1
 else:
-    toggle_state = 0
-    out_sigs['parallel_sink_input'][0] = 0b00000000
+    counter = 0
+
+    if toggle_state == 0:
+        toggle_state = 1
+        out_sigs['parallel_out'][0] = 0b10000000 # set pin 9 of parallel port to 1
+    else:
+        toggle_state = 0
+        out_sigs['parallel_out'][0] = 0b00000000
