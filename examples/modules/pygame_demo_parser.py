4f43d712afb6274afd175f4ea5d5aadafe79e2f1
Paul Nuyujukian <38359213+bil-paul@users.noreply.github.com>
2018-08-10 20:12:26 -0700

    pull in pygame visualization code and example model

---
+++ examples/modules/pygame_demo_parser.py
@@ -0,0 +1,10 @@
+if not pNumTicks[0] % refresh_rate:
+
+    theta += math.pi/64
+
+    pos = (r * math.cos(theta) + offset[0], r * math.sin(2*theta) + offset[1])
+    cir1.set_pos(pos)
+
+    screen.fill(black)
+    sprites.draw(screen)
+    pygame.display.flip()
---
+++ install/vis_pygame_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# installs the packages needed for visualization with pygame
+
+# MUST be run while in the venv
+
+sudo apt-get -y install openbox xinit libsdl-dev lxterminal
+
+pip install pygame
