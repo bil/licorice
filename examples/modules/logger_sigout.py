97fe3d5ed6271dfcd2d05c07a0d4f0e88237b6e0
Sabar Dasgupta <s@bardasgupta.com>
2018-08-31 15:36:46 -0700

    fix logger_demo all signals

--- examples/modules/logger_sigout.py
+++ examples/modules/logger_sigout.py
@@ -4,6 +4,7 @@ if scalar_out[0] < 255:
 else:
     scalar_out[0] = 0
 
+vector_ints[:] = vector_intsRaw[vector_intsBufVars[8]][:]
 if vector_ints[0] < 0 :
     vector_ints[:] += 5
 else:
@@ -11,11 +12,13 @@ else:
     vector_ints[1] = -20000
     vector_ints[2] = -10000
 
+vector_floats[:] = vector_floatsRaw[vector_floatsBufVars[8]][:]
 if vector_floats[0] < 1e9 :
     vector_floats[:] *= 2
 else:
     vector_floats[:] = [1, 2, 3, 4]
 
+matrix_out[:] = matrix_outRaw[matrix_outBufVars[8]][:]
 if matrix_out[0, 0] < 1e10 :
     matrix_out *= 3
 else:
