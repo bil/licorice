524c613f0eac050f9d4acc3b695e7312bb185a74
Paul Nuyujukian <38359213+bil-paul@users.noreply.github.com>
2019-02-05 12:33:43 -0800

    updated db_ext_write.py to be a bit more robust and mindful or resources

--- templating/db_ext_write.py
+++ templating/db_ext_write.py
@@ -11,7 +11,7 @@ def createTables(cur, table) :
   else:
     dir_name = "code"
 
-  dir_path = os.getenv("BINARY_DIR") + "/" + dir_name
+  dir_path = os.path.join(os.getenv("BINARY_DIR"), dir_name)
   if not (os.path.exists(dir_path)):
     return
 
@@ -22,7 +22,7 @@ def createTables(cur, table) :
     if table is "metadata" :
       if filename[-5:] != ".yaml" : # check if filename is true yaml file
         continue
-    file_path = dir_path + "/" + filename
+    file_path = os.path.join(dir_path, filename)
     file_stat = os.stat(file_path)
     mtime = file_stat.st_mtime
     contents = yaml.safe_load(file(file_path))
@@ -37,14 +37,16 @@ while (True):
   out_dir_path = os.getenv("BINARY_DIR")
 
   # check all existing databases in directory
-  f = glob.glob(out_dir_path + "/*.db")
+  f = glob.glob(os.path.join)out_dir_path, data, ".db"))
   if not f :
+    time.sleep(5)
     continue
 
   # connect to most recently closed database
   u_index = f[0].rfind('_') # index of underscore
   indices = [int(i[u_index + 1 : -3]) for i in f] # database indices
   if max(indices) == 0 : # first database still being created
+    time.sleep(5)
     continue
   cur_index = indices.index(max(indices) - 1) # index in array corresponding to most recently closed db
 
