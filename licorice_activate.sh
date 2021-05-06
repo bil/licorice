d40a1360623adb695b2590a72f8cbae9cf324fc4
Peter Boennighausen <pboennig@ldev11.bil.stanford.edu>
2021-05-06 14:53:52 -0700

    paul updates to LICORICE_EXEC_STR

--- licorice_activate.sh
+++ licorice_activate.sh
@@ -14,15 +14,15 @@ then
   LICORICE_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
   LICORICE_TEMPLATING_DIR=$LICORICE_ROOT/templating
   #LICORICE_EXEC_STR="sudo PYTHONHOME=$VIRTUAL_ENV taskset 0x1 nice -n -20 ./timer"
-  LICORICE_EXEC_STR="sudo PYTHONHOME=$VIRTUAL_ENV nice -n -20 ./timer"
+  LICORICE_EXEC_STR="nice -n -20 ./timer"
   LICORICE_PS1_STR=LiCoRICE
-  
+
   EXPERIMENT_DIR=$LICORICE_ROOT/..
   MODEL_DIR=$EXPERIMENT_DIR/models
   BINARY_DIR=$EXPERIMENT_DIR/run/out
-  
+
   FILE_MODEL_NAME=$BINARY_DIR/model_name
-  
+
   # export env variables
   export LICORICE_ROOT
   export LICORICE_TEMPLATING_DIR
@@ -32,36 +32,36 @@ then
   export MODEL_DIR
   export BINARY_DIR
   export FILE_MODEL_NAME
-  
-  
+
+
   # parse LiCoRICE model
   licorice_parse_model() {
     pushd $LICORICE_TEMPLATING_DIR
     python lico_parse.py -m $MODEL_DIR/$1.yaml
     popd
-  
+
     printf "%s" "$1" > $FILE_MODEL_NAME
   }
-  
+
   # compile LiCoRICE model
   licorice_compile_model() {
     printf 'Compiling: %s\n\n' "`cat $FILE_MODEL_NAME`"
-  
+
     pushd $BINARY_DIR
     make clean
     make -j 2
     popd
   }
-  
+
   # run LiCoRICE model
   licorice_run_model() {
     printf 'Running: %s\n\n' "`cat $FILE_MODEL_NAME`"
-  
+
     pushd $BINARY_DIR
     $LICORICE_EXEC_STR
     popd
   }
-  
+
   # remove any compiled binaries
   licorice_wipe_binaries() {
     if [ -n "$BINARY_DIR" ]
@@ -71,12 +71,12 @@ then
       echo "WARNING: Could not find binaries directory. Not wiping anything."
     fi
   }
-  
+
   # remove shm mappings
   licorice_shm_clear() {
-    sudo rm -f /dev/shm/*
+    rm -f /dev/shm/*
   }
-  
+
   # parse, compile, & run LiCoRICE model
   licorice_go() {
     licorice_wipe_binaries
@@ -84,7 +84,7 @@ then
     licorice_compile_model
     licorice_run_model
   }
-  
+
   # deactivate licorice environemnt
   licorice_deactivate() {
 
@@ -98,7 +98,7 @@ then
     unset MODEL_DIR
     unset BINARY_DIR
     unset FILE_MODEL_NAME
-  
+
     unset -f licorice_parse_model
     unset -f licorice_compile_model
     unset -f licorice_run_model
@@ -107,7 +107,7 @@ then
     unset -f licorice_go
     unset -f licorice_deactivate
   }
-  
+
   export LICORICE_ROOT
   # export shell functions
   export -f licorice_parse_model
@@ -116,7 +116,7 @@ then
   export -f licorice_wipe_binaries
   export -f licorice_shm_clear
   export -f licorice_go
-  
+
   # change prompt
   PS1="[$LICORICE_PS1_STR] $PS1"
   export PS1
