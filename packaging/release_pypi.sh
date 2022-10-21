77382989733ea109002ef8d76a4e33aef4d25216
Sabar Dasgupta <s@bardasgupta.com>
2022-10-21 15:06:30 -0700

    Test PyPI Release

--- packaging/release_pypi.sh
+++ packaging/release_pypi.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Usage: Make sure setup.py is updated with the latest version number and
-# run this script from the default branch.
+# Usage: Make sure setup.py is updated with the latest version number
 
-GIT_CURRENT_BRANCH=`git branch --show-current`
-GIT_DEFAULT_BRANCH=`git remote show origin | sed -n '/HEAD branch/s/.*: //p'`
+cd $(dirname "$0")/..
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+GIT_CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+GIT_DEFAULT_BRANCH=`basename "$(git rev-parse --abbrev-ref origin/HEAD)"`
 GIT_VERSION_TAG="v$(python setup.py --version)"
+VERSION_REGEX="^v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+$"
 
 if [ $GIT_CURRENT_BRANCH != $GIT_DEFAULT_BRANCH ]; then
     echo "No action taken. Switch to default branch."
@@ -15,6 +20,11 @@ fi
 if [ git rev-parse $GIT_VERSION_TAG >/dev/null 2>&1 ]; then
     echo "No action taken. Tag exists."
     exit 1
+fi
+
+if [[ ! $GIT_VERSION_TAG =~ $VERSION_REGEX ]]; then
+    echo "No action taken. Invalid version number: $GIT_VERSION_TAG"
+    exit
 fi
 
 git tag $GIT_VERSION_TAG
--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 from sys import platform
+import re
 
 with open("install/requirements.in") as fh:
     # removes --no-binary and other options
-    install_requires = [dep.split(" ")[0] for dep in fh.read().splitlines()]
+    install_requires = [
+        re.sub(r"--.*", "", dep) for dep in fh.read().splitlines()
+    ]
 
 if "linux" in platform:
     with open("install/linux-requirements.in") as fh:
