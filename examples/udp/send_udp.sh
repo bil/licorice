8992f065a763dcbc34ff1b562343ec35e9892861
Sabar Dasgupta <s@bardasgupta.com>
2023-03-07 21:55:44 -0800

    Pytest fully passing in Darwin

--- examples/udp/send_udp.sh
+++ examples/udp/send_udp.sh
@@ -6,7 +6,7 @@ PORT=51002          # port number to read from (bind to)
 INTERFACE=localhost # interface name
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-  NC_OPTIONS=u
+  NC_OPTIONS=uc
 else
   NC_OPTIONS=4u
 fi
@@ -18,9 +18,9 @@ do
   # -u UDP
   # -w timeout (seconds)
   # -N    Shutdown the network socket after EOF on stdin
-  echo "STAN" | nc -$NC_OPTIONS -w 1  $INTERFACE $PORT
-  echo "FORD" | nc -$NC_OPTIONS -w 1  $INTERFACE $PORT
-  echo "TREE" | nc -$NC_OPTIONS -w 1  $INTERFACE $PORT
-  echo "MESS" | nc -$NC_OPTIONS -w 1  $INTERFACE $PORT
-  echo "AGES" | nc -$NC_OPTIONS -w 1  $INTERFACE $PORT
+  echo "STAN" | nc -$NC_OPTIONS -w 1 $INTERFACE $PORT
+  echo "FORD" | nc -$NC_OPTIONS -w 1 $INTERFACE $PORT
+  echo "TREE" | nc -$NC_OPTIONS -w 1 $INTERFACE $PORT
+  echo "MESS" | nc -$NC_OPTIONS -w 1 $INTERFACE $PORT
+  echo "AGES" | nc -$NC_OPTIONS -w 1 $INTERFACE $PORT
 done
